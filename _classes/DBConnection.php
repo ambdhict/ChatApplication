9e7537b375999a5fe4ed08892632b3bccafafeb3
ambdhict <adhict.222@gmail.com>
2023-06-11 17:52:26 +0800

    db connection

--- _classes/DBConnection.php
+++ _classes/DBConnection.php
@@ -2,7 +2,10 @@
     class DBConnection{
 
         function connection(){
-            $con = new PDO("mysql:host=localhost;dbname=u372934681_dbchatsystem", "u372934681_admin", "Admin123");
+            // live connection for database
+            // $con = new PDO("mysql:host=localhost;dbname=u372934681_dbchatsystem", "u372934681_admin", "Admin123");
+            // local connection for database
+            $con = new PDO("mysql:host=localhost;dbname=dbchatsystem", "root", "");
 
             return $con;
         }
